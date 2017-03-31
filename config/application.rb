eddf0f42dfe92d4a7fb5805ae3fce7fda7ba6f80
jiunjiun <quietmes@gmail.com>
2017-04-01 01:08:04 +0800

    Update - Update application.rb and add locales

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,19 @@ module Whale
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.default_locale = 'zh-TW'
+    config.time_zone = 'Taipei'
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
