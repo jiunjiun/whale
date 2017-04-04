53415b81b9d1c176e8dc3a70dca0606450ecfc6a
jiunjiun <quietmes@gmail.com>
2017-04-04 16:40:48 +0800

    Add - Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User
+user = User.create email: 'user@user.com', password: '12345678', name: 'haha'
+
+# Shark
+shark = user.sharks.create name: 'Bot', cetacea: KeyValues::Shark::Cetacea.find_by_code('comic').id, desc: 'hahahah', web_url: '', fb_url: '', gh_url: ''
+
+# Bots
+shark.bots.create kind: KeyValues::Bot::Kind.find_by_code('facebook').id, bot_id: 'facebook_bot'
+shark.bots.create kind: KeyValues::Bot::Kind.find_by_code('line').id, bot_id: 'line_bot'
