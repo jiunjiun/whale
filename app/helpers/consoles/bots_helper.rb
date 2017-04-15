5aecb5605e669fd424cc15366744b3057ba8f659
jiunjiun <quietmes@gmail.com>
2017-04-16 01:20:44 +0800

    Refine - Refine developers

--- app/helpers/consoles/bots_helper.rb
+++ app/helpers/consoles/bots_helper.rb
@@ -1,17 +1,21 @@
 module Consoles::BotsHelper
   def bot_collection
     bot_kind_ids = KeyValues::Bot::Kind.all.map(&:id)
-    bots_ids     = bots.pluck(:id)
+    bots_ids     = bots.pluck(:kind)
 
     KeyValues::Bot::Kind.find(bot_kind_ids - bots_ids).map {|t| [t.name, t.id]}
   end
 
   def bot_link_to bot
+    link_to bot_link(bot), bot_link(bot), target: '_blank'
+  end
+
+  def bot_link bot
     case bot.kind
     when Bot::Kind::FACEBOOK
-      link_to "https://m.me/#{bot.bot_id}", "https://m.me/#{bot.bot_id}", target: '_blank'
+      "https://m.me/#{bot.bot_id}"
     when Bot::Kind::LINE
-      link_to "https://line.me/R/ti/p/#{bot.bot_id}", "https://line.me/R/ti/p/#{bot.bot_id}", target: '_blank'
+      "https://line.me/R/ti/p/#{bot.bot_id}"
     when Bot::Kind::TELEGRAM
     end
   end
