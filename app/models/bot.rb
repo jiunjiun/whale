1a7accb666dfb5c824a1edc055c5bd46031af398
jiunjiun <quietmes@gmail.com>
2017-04-21 00:37:17 +0800

    Refine - Refine bot

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -4,9 +4,16 @@ class Bot < ApplicationRecord
   validates_presence_of :kind, :bot_id
   validates_uniqueness_of :kind, scope: :shark_id
 
+  before_create :clear_space
+
   module Kind
     FACEBOOK = 1
     LINE     = 2
     TELEGRAM = 3
+  end
+
+  private
+  def clear_space
+    self.bot_id.delete!(' ')
   end
 end
