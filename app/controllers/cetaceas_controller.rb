5469170b46fb84849c707521e0eac2660b51151e
jiunjiun <quietmes@gmail.com>
2017-04-21 17:28:27 +0800

    Refine - Refine cetaceas order

--- app/controllers/cetaceas_controller.rb
+++ app/controllers/cetaceas_controller.rb
@@ -1,5 +1,5 @@
 class CetaceasController < ApplicationController
-  expose :sharks, {find: ->(id, scope) { scope.where(status: Shark::Status::RELEASE, cetacea: id) }}
+  expose :sharks, {find: ->(id, scope) { scope.where(status: Shark::Status::RELEASE, cetacea: id).order(updated_at: :desc) }}
 
   def show
     redirect_to root_path and return if KeyValues::Shark::Cetacea.where(id: params[:id].to_i).blank?
