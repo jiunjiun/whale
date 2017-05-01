8951e3355ebe1b96f29e0dcd06f1f570c41fd94a
jiunjiun <quietmes@gmail.com>
2017-05-01 22:44:55 +0800

    Refine - Refine something

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,5 +1,21 @@
 class HelpsController < ApplicationController
 
   def donate
+    opts = {
+      site: I18n.t('helps.donate'),
+      description: I18n.t('helps.donate_desc'),
+    }
+
+    update_meta opts
+  end
+
+  private
+  def update_meta opts
+    meta_tags_option = {
+      site: opts[:site],
+      description: opts[:description],
+    }
+
+    prepare_meta_tags meta_tags_option
   end
 end
