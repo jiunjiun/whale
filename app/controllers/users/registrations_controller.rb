54d681e1889b62e5f3f560c9c96a784ff8de21cb
jiunjiun <quietmes@gmail.com>
2017-05-01 23:18:10 +0800

    Refine - Refine SEO

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,17 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   layout 'sign_in'
+  before_action :update_meta, only: [:new]
 
   private
   def sign_up_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def update_meta
+    meta_tags_option = {
+      site: I18n.t('sign_up'),
+    }
+
+    prepare_meta_tags meta_tags_option
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,3 +1,13 @@
 class Users::SessionsController < Devise::SessionsController
   layout 'sign_in'
+  before_action :update_meta, only: [:new]
+
+  private
+  def update_meta
+    meta_tags_option = {
+      site: I18n.t('sign_in'),
+    }
+
+    prepare_meta_tags meta_tags_option
+  end
 end
