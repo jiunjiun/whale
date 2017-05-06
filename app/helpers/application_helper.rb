a5b58c8a41ddb15cb192131ea25c679a095fe64c
alanyeh20001 <alanyeh20001@hotmail.com>
2017-05-06 14:46:15 +0800

    Apply user GA code in specific pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def render_user_google_analytics_snippet
+    if user_tracking_page?
+      render "common/google_analytics", tracking_id: tracking_id if tracking_id
+    end
+  end
+
+  def user_tracking_page?
+    (controller_name == "sharks" && (action_name == "show" || "donate")) ||
+    (controller_name == "developers" && action_name == "show")
+  end
+
+  def tracking_id
+    case controller_name
+    when "sharks" then shark.user.tracking_id
+    when "developers" then user.tracking_id
+    end
+  end
 end
