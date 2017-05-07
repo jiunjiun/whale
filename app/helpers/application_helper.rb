ad29b91636a4fd2f436791f868814f1342a1247b
jiunjiun <quietmes@gmail.com>
2017-05-07 17:49:21 +0800

    Refine - Refine GA

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,21 @@
 module ApplicationHelper
-  def render_user_google_analytics_snippet
-    if user_tracking_page?
-      render "common/google_analytics", tracking_id: tracking_id if tracking_id
+  def developer_google_analytics_snippet
+    if Rails.env.production? && user_tracking_page? && tracking_id
+      render "common/google_analytics", tracking_id: tracking_id
     end
   end
 
   def user_tracking_page?
-    (controller_name == "sharks" && (action_name == "show" || "donate")) ||
+    (controller_name == "sharks" && (action_name == "show" || action_name == "donate")) ||
     (controller_name == "developers" && action_name == "show")
   end
 
   def tracking_id
     case controller_name
-    when "sharks" then shark.user.tracking_id
-    when "developers" then user.tracking_id
+    when "sharks"
+      shark.user.tracking_id
+    when "developers"
+      user.tracking_id
     end
   end
 end
